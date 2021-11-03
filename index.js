e050dfaeab7d9927e5c1e8e569268968bc1c244c
jvredmoore <jvredmoore@gmail.com>
2021-11-03 14:53:54 +0000

    finshed code challenge

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //write your code here
+function reverse(str){  
+  let reversed = "";      
+   for (let i = str.length - 1; i >= 0; i--){         
+     reversed += str[i];  
+   }     
+  return reversed;
+}
+
+console.log(reverse("hello"));
